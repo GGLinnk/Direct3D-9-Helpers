e3d02f2d7d19035b68f03144900379c77dea38c2
GGLinnk <gglinnk@protonmail.com>
2022-02-08 19:12:14 +0100

    Remove unwanted semicolon

--- pch.h
+++ pch.h
@@ -9,6 +9,6 @@
 
 // add headers that you want to pre-compile here
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-#include <d3d9.h>;
+#include <d3d9.h>
 
 #endif //PCH_H
